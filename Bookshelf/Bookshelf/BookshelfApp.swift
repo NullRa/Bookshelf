77e9c4a135ae5955357fa16c53352f35a0c51cf6
jyang <jTang@walkgame.com>
2022-01-28 10:26:17 +0800

    init project

---
+++ Bookshelf/Bookshelf/BookshelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookshelfApp.swift
+//  Bookshelf
+//
+//  Created by Apple on 2022/1/28.
+//
+
+import SwiftUI
+
+@main
+struct BookshelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookshelf/Bookshelf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookshelf
+//
+//  Created by Apple on 2022/1/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
